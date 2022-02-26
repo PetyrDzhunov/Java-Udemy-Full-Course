df497a148041a84f22e91567bf7dee3fcf77aaf9
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-02-26 12:56:48 +0200

    work with methods void or ones that return

--- ExpressionsStatementsCodeBlocksAndMethods/src/Methods.java
+++ ExpressionsStatementsCodeBlocksAndMethods/src/Methods.java
@@ -7,6 +7,10 @@ public class Methods {
 
         System.out.println(score);
         System.out.println(score1);
+
+       int position = calculateHighScorePosition(1500);
+        displayHighScorePosition("Peter",position);
+
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -16,6 +20,21 @@ public class Methods {
             return finalScore;
         }
         return -1;
+    }
 
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed to get into position " + position + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score > 1000) {
+            return 1;
+        } else if (score >= 500 && score <= 1000) {
+            return 2;
+        } else if (score < 500 && score >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
