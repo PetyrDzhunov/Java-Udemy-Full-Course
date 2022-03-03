bdc9bfe395fa476484f330b1c01147aaeef1043b
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-03 14:29:58 +0200

    add a new class that implements the same interface and understand what we can do with it

--- InnerAndAbstractClasses&Interfaces/src/com/company/Main.java
+++ InnerAndAbstractClasses&Interfaces/src/com/company/Main.java
@@ -4,10 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 //        ITelephone  johnsphone = new ITelephone(); - not valid.
-	ITelephone johnsPhone;
-    johnsPhone = new DeskPhone(123456);
-    johnsPhone.powerOn();
-    johnsPhone.callPhone(123456);
-    johnsPhone.answer();
-    }   
+        ITelephone johnsPhone;
+        johnsPhone = new DeskPhone(123456);
+        johnsPhone.powerOn();
+        johnsPhone.callPhone(123456);
+        johnsPhone.answer();
+
+        johnsPhone = new MobilePhone(24565);
+        johnsPhone.powerOn();
+        johnsPhone.callPhone(24565);
+        johnsPhone.answer();
+        // we can actually define and change a new instance of a different class
+        // and this works because they are both implementing the same interface.
+
+
+    }
 }
