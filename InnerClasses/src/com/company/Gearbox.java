0653898b26ed8d8f49a037879833ff66eaa750ba
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-03 17:25:45 +0200

    an example with a button with local class

--- InnerClasses/src/com/company/Gearbox.java
+++ InnerClasses/src/com/company/Gearbox.java
@@ -15,6 +15,10 @@ public class Gearbox {
         this.gears = new ArrayList<>();
         Gear neutral = new Gear(0, 0.0);
         this.gears.add(neutral);
+
+        for(int i=0; i<maxGears; i++) {
+            addGear(i, i* 5.3);
+        }
     }
 
     public void operateClitch(boolean in) {
@@ -39,14 +43,21 @@ public class Gearbox {
     public void changeGear(int newGear) {
         if ((newGear >= 0) && (newGear < this.gears.size()) && this.clutchIsIn) {
             this.currentGear = newGear;
-            System.out.println("Gear" + newGear + " selected");
+            System.out.println("Gear " + newGear + " selected");
         } else {
             System.out.println("Grind!");
             this.currentGear = 0;
         }
     }
 
-    // inner private class of Gearbox
+    // inner private class of Gearbox,local class
+    // this is an encapsulated class that only its outside class knows about
+    // you cant access this class anywhere outside. so its encapsulated  and
+    // it helps only the main class here
+    // local classes are declared inside a block such a method, or if/else statement
+    // their scope is restricted completely to that particular block,
+    // local classes are used less often and they increase encapsulation
+    // in practice we don't use them often
     private class Gear {
         private int gearNumber;
         private double ratio;
--- InnerClasses/src/com/company/Main.java
+++ InnerClasses/src/com/company/Main.java
@@ -1,15 +1,55 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
 
     public static void main(String[] args) {
 
         Gearbox mcLaren = new Gearbox(6);
-        // instantiate a gear object is only possible with an instance of Gearbox
-//        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-        System.out.println(first.driveSpeed(1000));
+        mcLaren.operateClitch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClitch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClitch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClitch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
 
-//        Gearbox.Gear second = new Gearbox.Gear(2,15.4); // not possible, because not an instance of Gearbox.
-//        Gearbox.Gear third  = new mcLaren.Gear(3, 17.8); // also not possible.
+        // create a local class that implements that onClickListener interface
+        // and assign an instance to our button;
+        class ClickListener implements Button.OnClickListener {
+            public ClickListener() {
+                System.out.println("I've been attached");
+            }
+
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+
+        }
+
+        btnPrint.setOnClickListener(new ClickListener());
+        listen();
+    }
+
+    private static void listen() {
+        boolean quit = false;
+        while (!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
     }
 }
