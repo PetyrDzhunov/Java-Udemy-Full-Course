384a0488c46f1b7ad8a6cb8db2ea4a36ac24f4fb
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-03 18:01:36 +0200

    abstract classes

--- AbstractClass/src/com/company/Parrot.java
+++ AbstractClass/src/com/company/Parrot.java
@@ -1,12 +1,8 @@
 package com.company;
 
-public class Parrot  extends  Bird{
+public class Parrot extends Bird {
     public Parrot(String name) {
         super(name);
     }
 
-    @Override
-    public void fly() {
-        System.out.println("Flitting from branch to branch");
-    }
 }
